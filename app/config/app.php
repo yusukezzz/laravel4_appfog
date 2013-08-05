b00a051d810469c30b18bc28d5c14198fad972bc
yusukezzz <g0632198@gmail.com>
2013-08-06 00:53:34 +0900

    add profiler, test packages

--- app/config/app.php
+++ app/config/app.php
@@ -113,6 +113,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'Juy\Profiler\Providers\ProfilerServiceProvider',
 
 	),
 
@@ -178,6 +179,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+		'Profiler'        => 'Juy\Profiler\Facades\Profiler',
 
 	),
 
